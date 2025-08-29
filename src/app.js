0d47917ceed689cc97e53ff424abcab7c8ff41c6
SoFTeR1 <softer110@gmail.com>
2025-08-29 19:24:18 +0300

    fix: Add missing comma in CORS config

--- src/app.js
+++ src/app.js
@@ -9,10 +9,9 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: [
-    'http://127.0.0.1:8080', // Адрес вашего Live Server
-    'http://localhost:8080', // Иногда Live Server использует этот адрес
-	'https://quantum-opal.vercel.app'
-    // Можно добавить и другие адреса, если потребуется
+    'http://127.0.0.1:8080',
+    'http://localhost:8080', // <--- ДОБАВЬТЕ ЭТУ ЗАПЯТУЮ
+    'https://quantum-opal.vercel.app' 
   ]
 }));
 app.use(express.json());
